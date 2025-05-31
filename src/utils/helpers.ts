bce53345a2ee73fb21c54cb113fea3c374293404
Nitesh <niteshprimedev@gmail.com>
2025-05-31 18:44:13 +0530

    Working on Pathfinding Context & CreateGrid setup is done

---
+++ src/utils/helpers.ts
@@ -0,0 +1,30 @@
+import { type TileType, type GridType } from "./types"
+import { MAX_ROWS, MAX_COLS } from "./constants"
+
+const createRow = (row: number, startTile: TileType, endTile: TileType) => {
+    const currentRow: array = [];
+    for (let col = 0; col <= MAX_COLS; col++) {
+        currentRow.push({
+            row,
+            col,
+            isEnd: (row === endTile.row && col === endTile.col),
+            isWall: false,
+            isPath: false,
+            distance: Infinity,
+            isStart: (row === startTile.row && col === startTile.row),
+            isTraversed: false,
+            parent: null,
+        });
+    }
+
+    return currentRow;
+}
+
+export const createGrid = (startTile: TileType, endTile: TileType) => {
+    const grid: GridType = [];
+    for (let row = 0; row <= MAX_ROWS; row++) {
+        grid.push(createRow(row, startTile, endTile));
+    }
+
+    return grid;
+}
