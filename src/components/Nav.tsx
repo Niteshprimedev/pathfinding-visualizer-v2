b00d01e03ac63826e2fa888b158c6b6e40424549
Nitesh <niteshprimedev@gmail.com>
2025-07-21 12:12:41 +0530

    Fixed type issues and Variables typo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,113 +1,137 @@
-import Select from './Select';
-import PlayButton from '../components/PlayButton'
-import { EXTENDED_SLEEP_TIME, MAZES, PATHFINDING_ALGORITHMS, SLEEP_TIME, SPEEDS } from '../utils/constants';
-import { usePathfinding } from '../hooks/usePathfinding';
-import type { AlgorithmType, MazeType, SpeedType } from '../utils/types';
-import { resetGrid } from '../utils/resetGrid';
-import { useTile } from '../hooks/useTile';
-import { useState, RefObject } from 'react';
-import { runMazeAlgorithm } from '../utils/runMazeAlgorithms';
-import { useSpeed } from '../hooks/useSpeed';
-import { runPathFindingAlgorithms } from '../utils/runPathFindingAlgorithms';
-import { animatePath } from '../utils/animatePath';
+import Select from "./Select";
+import PlayButton from "../components/PlayButton";
+import {
+  EXTENDED_SLEEP_TIME,
+  MAZES,
+  PATHFINDING_ALGORITHMS,
+  SLEEP_TIME,
+  SPEEDS,
+} from "../utils/constants";
+import { usePathfinding } from "../hooks/usePathfinding";
+import type { AlgorithmType, MazeType, SpeedType } from "../utils/types";
+import { resetGrid } from "../utils/resetGrid";
+import { useTile } from "../hooks/useTile";
+import { useState, type RefObject } from "react";
+import { runMazeAlgorithm } from "../utils/runMazeAlgorithms";
+import { useSpeed } from "../hooks/useSpeed";
+import { runPathFindingAlgorithms } from "../utils/runPathFindingAlgorithms";
+import { animatePath } from "../utils/animatePath";
 
-function Nav({ isVisualizationRunningRef }: { isVisualizationRunningRef: RefObject<boolean> }) {
-    const [isDisabled, setIsDisabled] = useState(false);
-    const { maze, setMaze, grid, setGrid, isGraphVisualized, setIsGraphVisualized, algorithm, setAlgorithm } = usePathfinding();
-    const { startTile, endTile } = useTile();
-    const { speed, setSpeed } = useSpeed();
+function Nav({
+  isVisualizationRunningRef,
+}: {
+  isVisualizationRunningRef: RefObject<boolean>;
+}) {
+  const [isDisabled, setIsDisabled] = useState(false);
+  const {
+    maze,
+    setMaze,
+    grid,
+    setGrid,
+    isGraphVisualized,
+    setIsGraphVisualized,
+    algorithm,
+    setAlgorithm,
+  } = usePathfinding();
+  const { startTile, endTile } = useTile();
+  const { speed, setSpeed } = useSpeed();
 
-    const handleGenerateMaze = (maze: MazeType) => {
-        console.log(maze, 'Handle Maze Component Clicked');
-        if (maze === "NONE") {
-            setMaze(maze);
-            resetGrid({ grid, startTile, endTile });
-            return;
-        }
-
-        setMaze(maze);
-        setIsDisabled(true);
-        runMazeAlgorithm({
-            maze,
-            grid,
-            startTile,
-            endTile,
-            setIsDisabled,
-            speed,
-        });
-        const newGrid = grid.slice();
-        setGrid(newGrid);
-        setIsGraphVisualized(false);
+  const handleGenerateMaze = (maze: MazeType) => {
+    console.log(maze, "Handle Maze Component Clicked");
+    if (maze === "NONE") {
+      setMaze(maze);
+      resetGrid({ grid, startTile, endTile });
+      return;
     }
 
-    const handleRunVisualizer = () => {
-        if (isGraphVisualized) {
-            setIsGraphVisualized(false);
-            resetGrid({ grid: grid.slice(), startTile, endTile });
-            return;
-        }
+    setMaze(maze);
+    setIsDisabled(true);
+    runMazeAlgorithm({
+      maze,
+      grid,
+      startTile,
+      endTile,
+      setIsDisabled,
+      speed,
+    });
+    const newGrid = grid.slice();
+    setGrid(newGrid);
+    setIsGraphVisualized(false);
+  };
 
-        // run the algorithm;
-        const { traversedTiles, path } = runPathFindingAlgorithms({
-            algorithm,
-            grid,
-            startTile,
-            endTile
-        });
-
-        animatePath(traversedTiles, path, startTile, endTile, speed);
-        setIsDisabled(true);
-        isVisualizationRunningRef.current = true;
-        setTimeout(() => {
-            const newGrid = grid.slice();
-            setGrid(newGrid);
-            setIsGraphVisualized(true);
-            setIsDisabled(false);
-            isVisualizationRunningRef.current = false;
-        }, (SLEEP_TIME * (traversedTiles.length + SLEEP_TIME * 2) + EXTENDED_SLEEP_TIME * (path.length + 60) * SPEEDS.find((s) => s.value === speed)!.value));
-        // console.log(traversedTiles, path);
+  const handleRunVisualizer = () => {
+    if (isGraphVisualized) {
+      setIsGraphVisualized(false);
+      resetGrid({ grid: grid.slice(), startTile, endTile });
+      return;
     }
 
-    let count = 0;
-    // console.log("Nav component Rendered", count++);
-    return (
-        <div className='flex items-center justify-center min-h-[4.5rem] border-b shadow-gray-600 sm:px-5 px-0'>
-            <div className='flex items-center lg:justify-between justify-center w-full sm:w-[52rem]'>
-                <h1 className='lg:flex hidden w-[40%] text-2xl pl-1'>
-                    Pathfinding Visualizer
-                </h1>
-                <div className="flex sm:items-end items-center justify-start sm:justify-between sm:flex-row flex-col sm:space-y-0 space-y-3 sm:py-0 py-4 sm:space-x-4">
-                    <Select
-                        value={maze}
-                        label='Maze'
-                        options={MAZES}
-                        onChange={(e) => { handleGenerateMaze(e.target.value as MazeType) }}
-                        isDisabled={false}
-                    ></Select>
-                    <Select
-                        value={algorithm}
-                        label='Graph'
-                        options={PATHFINDING_ALGORITHMS}
-                        onChange={(e) => { setAlgorithm(e.target.value as AlgorithmType) }}
-                        isDisabled={false}
-                    ></Select>
-                    <Select
-                        value={speed}
-                        label='Speed'
-                        options={SPEEDS}
-                        onChange={(e) => { setSpeed(parseInt(e.target.value) as SpeedType) }}
-                        isDisabled={false}
-                    >
-                    </Select>
-                    <PlayButton
-                        isDisabled={isDisabled}
-                        isGraphVisualized={isGraphVisualized}
-                        handleRunVisualizer={handleRunVisualizer}
-                    ></PlayButton>
-                </div>
-            </div>
-        </div >
-    )
+    // run the algorithm;
+    const { traversedTiles, path } = runPathFindingAlgorithms({
+      algorithm,
+      grid,
+      startTile,
+      endTile,
+    });
+
+    animatePath(traversedTiles, path, startTile, endTile, speed);
+    setIsDisabled(true);
+    isVisualizationRunningRef.current = true;
+    setTimeout(() => {
+      const newGrid = grid.slice();
+      setGrid(newGrid);
+      setIsGraphVisualized(true);
+      setIsDisabled(false);
+      isVisualizationRunningRef.current = false;
+    }, SLEEP_TIME * (traversedTiles.length + SLEEP_TIME * 2) + EXTENDED_SLEEP_TIME * (path.length + 60) * SPEEDS.find((s) => s.value === speed)!.value);
+    // console.log(traversedTiles, path);
+  };
+
+  //   let count = 0;
+  // console.log("Nav component Rendered", count++);
+  return (
+    <div className="flex items-center justify-center min-h-[4.5rem] border-b shadow-gray-600 sm:px-5 px-0">
+      <div className="flex items-center lg:justify-between justify-center w-full sm:w-[52rem]">
+        <h1 className="lg:flex hidden w-[40%] text-2xl pl-1">
+          Pathfinding Visualizer
+        </h1>
+        <div className="flex sm:items-end items-center justify-start sm:justify-between sm:flex-row flex-col sm:space-y-0 space-y-3 sm:py-0 py-4 sm:space-x-4">
+          <Select
+            value={maze}
+            label="Maze"
+            options={MAZES}
+            onChange={(e) => {
+              handleGenerateMaze(e.target.value as MazeType);
+            }}
+            isDisabled={false}
+          ></Select>
+          <Select
+            value={algorithm}
+            label="Graph"
+            options={PATHFINDING_ALGORITHMS}
+            onChange={(e) => {
+              setAlgorithm(e.target.value as AlgorithmType);
+            }}
+            isDisabled={false}
+          ></Select>
+          <Select
+            value={speed}
+            label="Speed"
+            options={SPEEDS}
+            onChange={(e) => {
+              setSpeed(parseInt(e.target.value) as SpeedType);
+            }}
+            isDisabled={false}
+          ></Select>
+          <PlayButton
+            isDisabled={isDisabled}
+            isGraphVisualized={isGraphVisualized}
+            handleRunVisualizer={handleRunVisualizer}
+          ></PlayButton>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
--- src/lib/algorithms/pathfinding/runAStarAlgo.ts
+++ src/lib/algorithms/pathfinding/runAStarAlgo.ts
@@ -1,68 +1,71 @@
-import { CgUserRemove } from "react-icons/cg";
+// import { CgUserRemove } from "react-icons/cg";
 import { getUntraversedNeighbors } from "../../../utils/getUntraversedNeighbors";
 import { dropFromQueue, isEqual } from "../../../utils/helpers";
 import { initFunctionCost, initHeuristicCost } from "../../../utils/heuristics";
 import type { GridType, TileType } from "../../../utils/types";
 
 export const runAStarAlgo = (
-    grid: GridType,
-    startTile: TileType,
-    endTile: TileType
+  grid: GridType,
+  startTile: TileType,
+  endTile: TileType
 ) => {
-    const traversedTiles: TileType[] = [];
+  const traversedTiles: TileType[] = [];
 
-    const heuristicCost = initHeuristicCost(grid, endTile);
-    const functionCost = initFunctionCost();
+  const heuristicCost = initHeuristicCost(grid, endTile);
+  const functionCost = initFunctionCost();
 
-    const base = grid[startTile.row][startTile.col];
-    base.distance = 0;
-    functionCost[base.row][base.col] = base.distance + heuristicCost[base.row][base.col];
+  const base = grid[startTile.row][startTile.col];
+  base.distance = 0;
+  functionCost[base.row][base.col] =
+    base.distance + heuristicCost[base.row][base.col];
 
-    base.isTraversed = true;
+  base.isTraversed = true;
 
-    const unTraversedTiles = [base];
+  const unTraversedTiles = [base];
 
-    while (unTraversedTiles.length > 0) {
-        unTraversedTiles.sort((a: TileType, b: TileType) => {
-            if (functionCost[a.row][a.col] === functionCost[b.row][b.col]) {
-                return b.distance - a.distance;
-            }
-            return functionCost[a.row][a.col] - functionCost[b.row][b.col];
-        });
+  while (unTraversedTiles.length > 0) {
+    unTraversedTiles.sort((a: TileType, b: TileType) => {
+      if (functionCost[a.row][a.col] === functionCost[b.row][b.col]) {
+        return b.distance - a.distance;
+      }
+      return functionCost[a.row][a.col] - functionCost[b.row][b.col];
+    });
 
-        const currentTile = unTraversedTiles.shift() as TileType;
+    const currentTile = unTraversedTiles.shift() as TileType;
 
-        if (currentTile.isWall === true) {
-            continue;
-        }
-        if (currentTile.distance === Infinity) break;
-
-        currentTile.isTraversed = true;
-        traversedTiles.push(currentTile);
-
-        if (isEqual(currentTile, endTile)) break;
-
-        const neighbors = getUntraversedNeighbors(grid, currentTile);
-
-        for (let idxI = 0; idxI < neighbors.length; idxI++) {
-            const distanceToNeighbor = currentTile.distance + 1;
-            if (distanceToNeighbor < neighbors[idxI].distance) {
-                dropFromQueue(neighbors[idxI], unTraversedTiles);
-                neighbors[idxI].distance = distanceToNeighbor;
-                functionCost[neighbors[idxI].row][neighbors[idxI].col] = neighbors[idxI].distance + heuristicCost[neighbors[idxI].row][neighbors[idxI].col];
-                neighbors[idxI].parent = currentTile;
-                unTraversedTiles.push(neighbors[idxI]);
-            }
-        }
+    if (currentTile.isWall === true) {
+      continue;
     }
+    if (currentTile.distance === Infinity) break;
 
-    const path = [];
-    let tile = grid[endTile.row][endTile.col];
-    while (tile !== null) {
-        tile.isPath = true;
-        path.unshift(tile);
-        tile = tile.parent!;
+    currentTile.isTraversed = true;
+    traversedTiles.push(currentTile);
+
+    if (isEqual(currentTile, endTile)) break;
+
+    const neighbors = getUntraversedNeighbors(grid, currentTile);
+
+    for (let idxI = 0; idxI < neighbors.length; idxI++) {
+      const distanceToNeighbor = currentTile.distance + 1;
+      if (distanceToNeighbor < neighbors[idxI].distance) {
+        dropFromQueue(neighbors[idxI], unTraversedTiles);
+        neighbors[idxI].distance = distanceToNeighbor;
+        functionCost[neighbors[idxI].row][neighbors[idxI].col] =
+          neighbors[idxI].distance +
+          heuristicCost[neighbors[idxI].row][neighbors[idxI].col];
+        neighbors[idxI].parent = currentTile;
+        unTraversedTiles.push(neighbors[idxI]);
+      }
     }
+  }
 
-    return { traversedTiles, path };
-}
+  const path = [];
+  let tile = grid[endTile.row][endTile.col];
+  while (tile !== null) {
+    tile.isPath = true;
+    path.unshift(tile);
+    tile = tile.parent!;
+  }
+
+  return { traversedTiles, path };
+};
